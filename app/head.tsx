f1f5beecc7280a8d1a11bd104bb66bf28952cd20
Bartek Paczesny <bartek@paczesny.pl>
2023-03-28 14:58:14 +0200

    Add: google analytics

--- app/head.tsx
+++ app/head.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 export default function Head() {
     return (
         <>
@@ -9,6 +11,20 @@ export default function Head() {
             <meta name="description" content="Bartek Paczesny - Portfolio" />
             <meta name="theme-color" content="#001120" />
             <link rel="icon" href="/favicon.ico" />
+            <Script
+                strategy="lazyOnload"
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            />
+            <Script id="1" strategy="lazyOnload">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                        page_path: window.location.pathname,
+                    });
+                `}
+            </Script>
         </>
     );
 }
