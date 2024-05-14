ab33749bd200141a360117b22cbbbb956f899277
Bartek Paczesny <bartek@paczesny.pl>
2024-05-14 22:05:42 +0200

    asd

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
 import Script from "next/script";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/react";
 
 import { NextUIProvider } from "@nextui-org/react";
@@ -14,13 +14,16 @@ import { NextUIProvider } from "@nextui-org/react";
 export function Providers({ children, ...props }: ThemeProviderProps) {
     const router = useRouter();
 
+    const gaId: string = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!;
+
     return (
         <NextUIProvider navigate={router.push}>
             <NextThemesProvider {...props}>
                 <Analytics />
-                <GoogleAnalytics
-                    gaId={`${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-                />
+                <GoogleTagManager gtmId="GT-P8Q25N2X" />
+                {/* <GoogleAnalytics
+                    gaId={gaId}
+                /> */}
                 {children}
             </NextThemesProvider>
         </NextUIProvider>
