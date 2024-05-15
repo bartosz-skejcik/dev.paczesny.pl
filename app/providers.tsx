c58de4fb3d2e7259b74a6d667a7936a2dad146e8
Bartek Paczesny <bartek@paczesny.pl>
2024-05-16 01:05:29 +0200

    chore: Add @vercel/speed-insights for performance monitoring

--- app/providers.tsx
+++ app/providers.tsx
@@ -7,7 +7,9 @@ import { type ThemeProviderProps } from "next-themes/dist/types";
 
 import Script from "next/script";
 import { GoogleAnalytics } from "@next/third-parties/google";
+
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { NextUIProvider } from "@nextui-org/react";
 
@@ -20,6 +22,7 @@ export function Providers({ children, ...props }: ThemeProviderProps) {
         <NextUIProvider navigate={router.push}>
             <NextThemesProvider {...props}>
                 <Analytics />
+                <SpeedInsights />
                 <GoogleAnalytics gaId={gaId} />
                 {children}
             </NextThemesProvider>
