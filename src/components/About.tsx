a9923e0d36ec2f8e14b1d482b34e0b3fe7800fc5
Bartek Paczesny <bartek@paczesny.pl>
2024-07-15 12:47:44 +0200

    chore: chane image on about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,13 +7,13 @@ import { motion } from "framer-motion";
 export default function About() {
     const images = [
         "/images/about/f1.jpeg",
-        "/images/about/stas.jpeg",
+        "/images/about/mscds.jpeg",
         "/images/about/coding.jpeg",
         "/images/about/coding1.jpeg",
     ];
     return (
         <div>
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-10 my-10">
+            <div className="my-10 grid grid-cols-2 gap-10 md:grid-cols-4">
                 {images.map((image, index) => (
                     <motion.div
                         key={image}
@@ -34,51 +34,51 @@ export default function About() {
                             width={200}
                             height={400}
                             alt="about"
-                            className="rounded-md object-cover transform rotate-3 shadow-xl block w-full h-40 md:h-60 hover:rotate-0 transition duration-200"
+                            className="block h-40 w-full rotate-3 transform rounded-md object-cover shadow-xl transition duration-200 hover:rotate-0 md:h-60"
                         />
                     </motion.div>
                 ))}
             </div>
 
             <div className="max-w-4xl">
-                <Paragraph className=" mt-4">
+                <Paragraph className="mt-4">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     Expedita quaerat itaque cupiditate dolor sit ullam nostrum
                     soluta voluptatem obcaecati earum!
                 </Paragraph>
-                <Paragraph className=" mt-4">
+                <Paragraph className="mt-4">
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                     Beatae doloremque magni dolore hic rem nam aspernatur totam
                     tempore. Ex voluptates asperiores minus ipsam corporis
                     quaerat cum distinctio qui, sit possimus!
                 </Paragraph>
 
-                <Paragraph className=" mt-4">
+                <Paragraph className="mt-4">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Quos mollitia illum quibusdam hic expedita nobis, odio rerum
                     voluptatibus, voluptate optio officiis obcaecati repudiandae
                     asperiores quo tempora enim libero accusantium quidem!
                 </Paragraph>
-                <Paragraph className=" mt-4">
+                <Paragraph className="mt-4">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
                     animi eligendi laboriosam sequi, nihil pariatur autem dolore
                     officia, et debitis illum quas quam. Laboriosam consectetur
                     eligendi dolorum quis quidem ducimus!
                 </Paragraph>
-                <Paragraph className=" mt-4">
+                <Paragraph className="mt-4">
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                     Iste, voluptate optio! Maxime, cumque iusto. Vitae mollitia
                     aliquid, cum omnis neque veritatis dolorem laudantium!
                     Incidunt dolore earum est porro impedit aspernatur.
                 </Paragraph>
-                <Paragraph className=" mt-4">
+                <Paragraph className="mt-4">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     Sed at et illum iste, placeat ab nesciunt iure similique
                     necessitatibus voluptatem exercitationem saepe totam
                     provident velit, tenetur impedit incidunt perspiciatis
                     deleniti.
                 </Paragraph>
-                <Paragraph className=" mt-4">
+                <Paragraph className="mt-4">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Blanditiis labore accusamus.
                 </Paragraph>
